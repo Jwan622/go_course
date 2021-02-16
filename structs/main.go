c396e5ff1354c68d2e76e347bb03bbf45e5d4a0f
Jeffrey <Jwan622@gmail.com>
2021-02-16 11:45:19 -0800

    more on structs

--- structs/main.go
+++ structs/main.go
@@ -8,6 +8,14 @@ type person struct {
 }
 
 func main() {
+	// one way to use structs
 	alex := person{firstName: "Alex", lastName: "Wan"}
 	fmt.Println(alex)
+
+	// second way to deal with structs
+	var alex2 person
+	alex2.firstName = "Alex"
+	alex2.lastName = "Wan22"
+	fmt.Println(alex2)
+	fmt.Printf("%+v", alex2)
 }
